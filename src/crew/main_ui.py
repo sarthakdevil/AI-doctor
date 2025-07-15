8e3224eac4332170d3cbc3dc5001576d1044c478
Sarthakdevil <Sarthakrajesh2005@gmail.com>
2025-07-15 12:20:30 +0530

    changes

--- src/crew/main_ui.py
+++ src/crew/main_ui.py
@@ -1,3 +1,7 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+sys.modules["sqlite3.dbapi2"] = sys.modules["pysqlite3.dbapi2"]
 #!/usr/bin/env python3
 """
 AI Doctor Crew - Streamlit Web Interface
